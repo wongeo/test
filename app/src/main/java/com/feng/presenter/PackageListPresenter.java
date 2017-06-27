11fa18e6d0617dbc61835026f1fde4dc60bc422b
wangjing <zhangjing@zhangyue.com>
2017-06-27 11:18:32 +0800

    a

---
+++ app/src/main/java/com/feng/presenter/PackageListPresenter.java
@@ -0,0 +1,14 @@
+package com.feng.presenter;
+
+import com.feng.fragment.PackageListFragment;
+import com.feng.mvp.BasePresenter;
+
+/**
+ * Created by feng on 2017/6/26.
+ */
+
+public class PackageListPresenter extends BasePresenter<PackageListFragment> {
+    public PackageListPresenter(PackageListFragment view) {
+        super(view);
+    }
+}
--- app/src/main/java/com/feng/test/MyFragment.java
+++ app/src/main/java/com/feng/test/MyFragment.java
@@ -2,6 +2,7 @@ package com.feng.test;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +18,7 @@ public class MyFragment extends BaseFragment<MyPresenter> implements View.OnClic
 
     private View mRootView;
 
+    private View mButton;
 
     public MyFragment() {
         MyPresenter presenter = new MyPresenter(this);
@@ -27,12 +29,19 @@ public class MyFragment extends BaseFragment<MyPresenter> implements View.OnClic
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         if (mRootView == null) {
             mRootView = inflater.inflate(R.layout.my_fragment, container, false);
+
         }
         return mRootView;
     }
 
     @Override
     public void onClick(View v) {
-    }
 
+        new AlertDialog.Builder(getActivity())
+                .setTitle("确认")
+                .setMessage("确定吗？")
+                .setPositiveButton("是", null)
+                .setNegativeButton("否", null)
+                .show();
+    }
 }
