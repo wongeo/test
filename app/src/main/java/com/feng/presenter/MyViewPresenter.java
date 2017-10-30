edbf98661ea7f27f5f4beb25cd23dd069939bded
WangJing <wj148202@alibaba-inc.com>
2017-10-30 19:12:41 +0800

    update

--- app/src/main/java/com/feng/presenter/MyViewPresenter.java
+++ app/src/main/java/com/feng/presenter/MyViewPresenter.java
@@ -1,18 +1,16 @@
 package com.feng.presenter;
 
 import com.feng.mvp.BasePresenter;
+import com.feng.mvp.BaseView;
+import com.feng.mvp.BaseViewPresenter;
 import com.feng.view.MyCustomView;
 
 /**
  * Created by wj148202 on 2017/10/30.
  */
 
-public class MyViewPresenter extends BasePresenter<MyCustomView> {
+public class MyViewPresenter extends BaseViewPresenter<MyCustomView> {
     public MyViewPresenter(MyCustomView view) {
         super(view);
-    }
-
-    public void abd(){
-        getView().isActivated()
     }
 }
--- app/src/main/java/com/feng/view/MyCustomView.java
+++ app/src/main/java/com/feng/view/MyCustomView.java
@@ -11,28 +11,26 @@ import com.feng.presenter.MyViewPresenter;
  * Created by wj148202 on 2017/10/30.
  */
 
-public class MyCustomView extends RelativeLayout implements BaseView<MyViewPresenter> {
+public class MyCustomView extends RelativeLayout implements BaseView {
 
     private MyViewPresenter mPresenter;
 
     public MyCustomView(Context context) {
         super(context);
+        init(context);
     }
 
     public MyCustomView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context);
     }
 
     public MyCustomView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context);
     }
 
-    private void inti() {
+    private void init(Context context) {
         mPresenter = new MyViewPresenter(this);
-    }
-
-    @Override
-    public void setPresenter(MyViewPresenter presenter) {
-        
     }
 }
